d99c2749655cef35bfb887797f551361daf1878e
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-10-03 09:39:35 -0300

    Registration Service

--- src/main/java/com/pedroacbg/api/register/registration/RegistrationService.java
+++ src/main/java/com/pedroacbg/api/register/registration/RegistrationService.java
@@ -1,12 +1,25 @@
 package com.pedroacbg.api.register.registration;
 
+import com.pedroacbg.api.register.appuser.AppUser;
+import com.pedroacbg.api.register.appuser.AppUserRole;
+import com.pedroacbg.api.register.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail =  emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(new AppUser(request.getFirstName(), request.getLastName(),
+                request.getEmail(), request.getPassword(), AppUserRole.USER));
     }
 
 }
